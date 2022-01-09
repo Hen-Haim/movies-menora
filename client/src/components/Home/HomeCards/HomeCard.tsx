292d2a5fb5f5dfbb52ae12450e990dc58257144b
Hen Haim <hen.haim222@gmail.com>
2022-01-09 15:33:26 +0200

    movie finished

--- client/src/components/Home/HomeCards/HomeCard.tsx
+++ client/src/components/Home/HomeCards/HomeCard.tsx
@@ -1,18 +1,26 @@
 import "./HomeCard.css";
-import { MovieModel } from '../../../Models/MovieModel'
-import { useDispatch } from 'react-redux';
-import { homeCardDescription } from '../../../store/actions';
+import { MovieModel } from "../../../Models/MovieModel";
+import { useDispatch } from "react-redux";
+import {
+  homeCardDescription,
+  ModalDetailsDescription,
+} from "../../../store/actions";
 
-export const HomeCard = (props: { movie?: MovieModel}) => {
-    const dispatch = useDispatch();
+export const HomeCard = (props: { movie?: MovieModel }) => {
+  const dispatch = useDispatch();
 
-    const ClickedMovie = () =>{
-        dispatch(homeCardDescription(props.movie?.imdbID));
-    }
+  const ClickedMovie = () => {
+    dispatch(homeCardDescription(props.movie?.imdbID));
+    dispatch(ModalDetailsDescription(props.movie?.imdbID));
+  };
 
-    return (
-        <div onClick={ClickedMovie} className="home-card">
-            <img className="home-card-img" src={props.movie?.Poster} alt = "home card movie poster"></img>
-        </div>
-    )
-}
+  return (
+    <div onClick={ClickedMovie} className="home-card">
+      <img
+        className="home-card-img"
+        src={props.movie?.Poster}
+        alt="home card movie poster"
+      ></img>
+    </div>
+  );
+};
--- client/src/store/actions.ts
+++ client/src/store/actions.ts
@@ -1,9 +1,8 @@
-import { MovieExtendedModel } from './../Models/MovieModel';
+import { MovieExtendedModel } from "./../Models/MovieModel";
 import axios from "axios";
 import { AllActionType } from "../Models/ActionsType";
 import { Dispatch } from "redux";
 import { MovieModel } from "../Models/MovieModel";
-
 
 export const sendMessage = (message: string) => (dispatch: Dispatch) => {
   dispatch({
@@ -20,15 +19,25 @@ export const emptyModal = () => async (dispatch: Dispatch) => {
 };
 
 export const emptyMovies = () => async (dispatch: Dispatch) => {
-    dispatch({
-      type: AllActionType.CLEAR_UP_MOVIES,
-    });
+  dispatch({
+    type: AllActionType.CLEAR_UP_MOVIES,
+  });
 };
 
-
 export const getTenMovies = () => async (dispatch: Dispatch) => {
-  const moviesArray = ["tt0451279", "tt2948372", "tt3631112", "tt11286314", "tt8579674", "tt12298506", "tt11274100", "tt3846674", "tt6841122", "tt7737786"]
-  for( let i=0; i<10; i++){
+  const moviesArray = [
+    "tt0451279",
+    "tt2948372",
+    "tt3631112",
+    "tt11286314",
+    "tt8579674",
+    "tt12298506",
+    "tt11274100",
+    "tt3846674",
+    "tt6841122",
+    "tt7737786",
+  ];
+  for (let i = 0; i < 10; i++) {
     try {
       const response = await axios.get<MovieModel>(
         `http://localhost:3001/movies/ten-movies/${moviesArray[i]}`
@@ -46,25 +55,27 @@ export const getTenMovies = () => async (dispatch: Dispatch) => {
   }
 };
 
-export const homeCardDescription = (movieId?: string) => async (dispatch: Dispatch) => {
-  movieId === undefined && (movieId = "tt0451279");
-  try {
-    const response = await axios.get<MovieModel>(
-      `http://localhost:3001/movies/home-card/${movieId}`
-    );
-    dispatch({
-      type: AllActionType.MOVIE_HOME_CARD,
-      payload: response.data,
-    });
-  } catch (error: any) {
-    dispatch({
-      type: AllActionType.MSG,
-      payload: error.message,
-    });
-  }
-};
+export const homeCardDescription =
+  (movieId?: string) => async (dispatch: Dispatch) => {
+    movieId === undefined && (movieId = "tt0451279");
+    try {
+      const response = await axios.get<MovieModel>(
+        `http://localhost:3001/movies/home-card/${movieId}`
+      );
+      dispatch({
+        type: AllActionType.MOVIE_HOME_CARD,
+        payload: response.data,
+      });
+    } catch (error: any) {
+      dispatch({
+        type: AllActionType.MSG,
+        payload: error.message,
+      });
+    }
+  };
 
-export const SearchExtendedDescription = (searchChosenMovie: string) => async (dispatch: Dispatch) => {
+export const SearchExtendedDescription =
+  (searchChosenMovie: string) => async (dispatch: Dispatch) => {
     try {
       const response = await axios.get<MovieExtendedModel>(
         `http://localhost:3001/movies/search-card/${searchChosenMovie}`
@@ -79,41 +90,40 @@ export const SearchExtendedDescription = (searchChosenMovie: string) => async (d
         payload: error.message,
       });
     }
-};
+  };
 
-export const ModalDetailsDescription = (id: string) => async (dispatch: Dispatch) => {
-  try {
-    const response = await axios.get<MovieExtendedModel>(
-      `http://localhost:3001/movies/modal/${id}`
-    );
-    dispatch({
-      type: AllActionType.MODAL,
-      payload: response.data,
-    });
-  } catch (error: any) {
-    dispatch({
-      type: AllActionType.MSG,
-      payload: error.message,
-    });
-  }
-};
+export const ModalDetailsDescription =
+  (id: string | undefined) => async (dispatch: Dispatch) => {
+    try {
+      const response = await axios.get<MovieExtendedModel>(
+        `http://localhost:3001/movies/modal/${id}`
+      );
+      dispatch({
+        type: AllActionType.MODAL,
+        payload: response.data,
+      });
+    } catch (error: any) {
+      dispatch({
+        type: AllActionType.MSG,
+        payload: error.message,
+      });
+    }
+  };
 
-export const movieChosenFromSearchComp = (id: string) => async (dispatch: Dispatch) => {
-  try {
-    const response = await axios.get<MovieExtendedModel>(
-      `http://localhost:3001/movies/modal/${id}`
-    );
-    dispatch({
-      type: AllActionType.MOVIE_SEARCH_CARD,
-      payload: response.data,
-    });
-  } catch (error: any) {
-    dispatch({
-      type: AllActionType.MSG,
-      payload: error.message,
-    });
-  }
-};
-
-
-
+export const movieChosenFromSearchComp =
+  (id: string) => async (dispatch: Dispatch) => {
+    try {
+      const response = await axios.get<MovieExtendedModel>(
+        `http://localhost:3001/movies/modal/${id}`
+      );
+      dispatch({
+        type: AllActionType.MOVIE_SEARCH_CARD,
+        payload: response.data,
+      });
+    } catch (error: any) {
+      dispatch({
+        type: AllActionType.MSG,
+        payload: error.message,
+      });
+    }
+  };
